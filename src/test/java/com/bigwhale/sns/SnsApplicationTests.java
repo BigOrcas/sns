7618ddca2b7a2ba2c85582464fc171996749b1f1
bigwhale1948@gmail.com <bigwhale1948@gmail.com>
2022-12-22 14:47:14 +0900

    [221222 프로젝트 생성]

---
+++ src/test/java/com/bigwhale/sns/SnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigwhale.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
